8739046260987acd6f910ece8134cb83c555e7d1
Pim Snel <pim@lingewoud.nl>
2021-02-12 16:42:54 +0100

    Update version to 0.2.4

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 )
-const version = "v0.2.3"
+const version = "v0.2.4"
 
 func setAuth(keyfilepath string, ignoreHostkey bool) transport.AuthMethod {
 	//var auth transport.AuthMethod
